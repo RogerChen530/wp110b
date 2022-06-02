68b284298db0955b347108f76847218673e3b83c
RogerChen530 <99713329+RogerChen530@users.noreply.github.com>
2022-06-02 16:44:59 +0800

    Delete 7th(trans-matrix).js

--- 7th(trans-matrix).js
+++
@@ -1,9 +0,0 @@
-const transpose=([[1,2,3], [3,2,1]]);
-const a=[];
-for(var i = 0 ; i < transpose[0].length ; i++){
-    a[i]=[];
-    for(var j = 0 ; j < transpose.length ; j++){
-        a[i][j] = transpose[j][i];
-    }
-}
-console.log(a);
